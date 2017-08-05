71da66d622240b5ebf91194012c4e9e262e5ccad
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2017-08-05 10:21:29 -0700

    Added styling to results page

--- SearchResults.js
+++ SearchResults.js
@@ -10,17 +10,86 @@ import {
 	Text,
 } from 'react-native';
 
+
+class ListItem extends React.PureComponent {
+	_onPress = () => {
+		this.props.onPressItem(this.props.index);
+	}
+
+	// manipulates the returned price which is in the format 300,000 GBP to remove the GBP suffix
+	// then it renders the row UI 
+	// an Image is added to the row and is loaded from a 
+	// returned URL item.img_url which React Native decodes
+	// off the main thread
+	render() {
+		const item = this.props.item;
+		const price = item.price_formatted.split(' ')[0];
+		return (
+			<TouchableHighlight
+				onPress={this._onPress}
+				underlayColor='#dddddd'>
+				<View>
+					<View style={styles.rowContainer}>
+						<Image style={styles.thumb} source={{ uri: item.img_url }} />
+						<View style={styles.textContainer}>
+							<Text style={styles.price}>{price}</Text>
+							<Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+						</View>
+	  			</View>
+  	 			<View style={styles.separator}/>
+  			</View>
+  		</TouchableHighlight>
+  	);
+  }
+}
+		
+
+
+
+
+
+// defines all styles that will be used to render each row
+const styles = StyleSheet.create({
+	thumb: {
+		width: 80,
+		height: 80,
+		marginRight: 10
+	},
+	textContainer: {
+		flex: 1
+	},
+	separator: {
+		height: 1,
+		backgroundColor: '#dddddd'
+	},
+	price: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#48BBEC'
+	},
+	title: {
+		fontSize: 20,
+		color: '#656565'
+	},
+	rowContainer: {
+		flexDirection: 'row',
+		padding: 10
+	},
+});
+
 export default class SearchResults extends Component {
 	_keyExtractor = (item, index) => index;
 	
-	_renderItem = ({item}) => {
-		return (
-			<TouchableHighlight underlayColor='#dddddd'>
-				<View>
-					<Text>{item.title}</Text>
-				</View>
-			</TouchableHighlight>
-		);
+	_renderItem = ({item, index}) => (
+		<ListItem
+			item={item}
+			index={index}
+			onPressItem={this._onPressItem}
+		/>
+	);
+
+	_onPressItem = (index) => {
+		console.log("Pressed row: "+index);
 	};
 
 	render() {
