432ff3da7c3219ef2ae95638e7ebae7f986ccb2e
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2017-08-05 08:53:50 -0700

    Added search box, initiating a search, functions for parsing an API request

--- index.ios.js
+++ index.ios.js
@@ -13,6 +13,7 @@
 // these moduls are assigned o variables called React and ReactNative
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
 
 
 // defines a single style that will be applied to "Hello World" text
@@ -22,25 +23,28 @@ var styles = ReactNative.StyleSheet.create({
 		backgroundColor: 'white',
 		fontSize: 30,
 		margin: 80
+	},
+	container: {
+		flex: 1
 	}
 });
 
 
-// the below is a JavaScript class
-// PropertyFinderApp extends React.Component -- this is the basic 
-// building block of the React UI
-// Components contain: 
-//    - immutable/unchangeable properties
-//		- mutable state variables and expose a method for rendering
-//		- hello world basic app only requires a render method
-// React Native components are lightweight equivalent to UIKit classes
-// 		- so the framework takes care of transforming the tree of
-//		React components into the required native UI
+
+// This constructs a navigation controller, applies a style, and sets the initial route to the HelloWorld component
 class PropertyFinderApp extends React.Component {
 	render() {
-		return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+		return (
+			<ReactNative.NavigatorIOS
+				style={styles.container}
+				initialRoute={{
+					title: 'Property Finder',
+					component: SearchPage,
+				}}/>
+		);
 	}
 }
+
 
 
 // AppRegistry defines the entry point to the application and provides the root component
